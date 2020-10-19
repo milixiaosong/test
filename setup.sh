1168d3871d0f9c6ef017608b8d67617f4d99eccc
小颂 <5153643+milixiaosong@user.noreply.gitee.com>
2020-10-19 09:56:00 +0800

    add setup.sh.

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+echo 'checking mysql status.'
+service mysql status
+ 
+echo '1.start mysql....'
+service mysql start
+sleep 3
+service mysql status
+ 
+echo '2.start importing data....'
+mysql < /mysql/pboot.sql
+echo '3.end importing data....'
+ 
+sleep 3
+service mysql status
+
+sleep 3
+service mysql status
+echo 'mysql is ready'
+
+tail -f /dev/null
