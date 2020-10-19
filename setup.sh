6f1b89dc40ca0c0b4ee813d6065ff5a63ed859e0
小颂 <5153643+milixiaosong@user.noreply.gitee.com>
2020-10-19 12:51:06 +0800

    update setup.sh.

--- setup.sh
+++ setup.sh
@@ -5,21 +5,21 @@ service mysql status
  
 echo '1.start mysql....'
 service mysql start
-sleep 3
+sleep 10s
 service mysql status
 
 echo "create database 'pboot'" | mysql -u username -p
-sleep 3
+sleep 3s
  
 echo '2.start importing data....'
 mysql pboot < /mysql/pboot.sql
 echo '3.end importing data....'
  
-sleep 3
+sleep 10s
 service mysql status
 
-sleep 3
-service mysql status
-echo 'mysql is ready'
+#sleep 3
+#service mysql status
+#echo 'mysql is ready'
 
-tail -f /dev/null
+#tail -f /dev/null
